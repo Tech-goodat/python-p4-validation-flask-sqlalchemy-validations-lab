42d83bbbd68134306c931b5777dc465e58d990de
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-11 19:06:32 +0300

    added validations

--- server/models.py
+++ server/models.py
@@ -12,6 +12,17 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name must be availlable!")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and len(phone_number) != 10:
+            raise ValueError("Phone number must be exactly ten numbers")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +39,29 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content_length(self, key, content):
+        if len(content) <250:
+            raise ValueError("conent must be more than 250 words")
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary)<250:
+            raise ValueError("summary must be more that 250 words")
+        return summary
+    @validates ('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('category must be either fiction or non viction')
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if not any(keyword in title for keyword in ["Won\'t Believe", "Secret", "Top", "Guess"]):
+            raise ValueError("heading must be within the specified keywords")
+        return title
+
+    
 
 
     def __repr__(self):
